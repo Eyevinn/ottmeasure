6226edae554a1c407a17330ab310bb6cfeb1a8cd
Jonas Birme <jonas.birme@gmail.com>
2014-05-27 12:08:20 +0200

    A rails skeleton project and MeasureService skeleton class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ottmeasure::Application.config.secret_token = 'fa61fc8ea6cf071a42361ba49109786555e33d191e96e60c34f91083ada34f20f676d5fd9f64d66f7e7dece646dc91ee60ecec6b8931e88696203221d8205d0c'
